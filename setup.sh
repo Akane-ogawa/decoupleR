390e69c93c245a4640f04e3b9e12ee10daee5905
小川茜 <ogawaakane@dhcp-190-123.rcai.riken.go.jp>
2025-09-11 15:03:50 +0900

    updated

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate decoupler_env
 
 # 特定のバージョンでインストール
-conda install numpy=2.2.6 scanpy=1.11.4 pandas=2.3.2 mygene=3.2.2
-pip install decoupler==2.1.1
+conda install numpy=2.2.6 pandas=2.3.2 
+pip install decoupler==2.1.1 scanpy=1.11.4 mygene=3.2.2
