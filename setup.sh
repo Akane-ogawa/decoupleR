49512e69cb4af2e0e0e79f24c5156b89b18faa59
小川茜 <ogawaakane@dhcp-190-123.rcai.riken.go.jp>
2025-09-11 14:18:17 +0900

    first commitment

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# 新しいAnaconda環境の作成
+conda create -n decoupler_env python=3.13.7 -y
+
+# 環境をアクティベート
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate decoupler_env
+
+# 特定のバージョンでインストール
+conda install numpy=2.2.6 scanpy=1.11.4 pandas=2.3.2 mygene=3.2.2
+pip install decoupler==2.1.1
